b0b5a45bd8699fef908f8390b7bc1fe33e41a0cc
Joaquin Vte. Alonso Saiz <jv.alonso@edu.gva.es>
2023-04-18 21:01:02 +0200

    Initial commit

--- app/src/main/java/com/example/myweatherbase/base/Parameters.java
+++ app/src/main/java/com/example/myweatherbase/base/Parameters.java
@@ -2,8 +2,8 @@ package com.example.myweatherbase.base;
 
 public class Parameters {
 
-    // Change the API KEY
-    public final static String API = "11beed541e7a0fd48076a05030a042ea";
+
+    public final static String API = "<put_your_API_Key_here>";
 
     public final static String LANG = "es";
     public final static String UNITS = "metric";
